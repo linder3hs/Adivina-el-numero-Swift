7a1b0b0d29d3a86ee6fef3146bc132da1f160a60
Linder Hassinger <linderhassinger00@gmail.com>
2018-02-14 13:18:11 -0500

    Login

--- start/Home.swift
+++ start/Home.swift
@@ -8,7 +8,35 @@
 
 import Foundation
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class HomeController: UIViewController {
     
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+
+    @IBAction func loginButton(_ sender: Any) {
+        if self.username.text == "" || self.password.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Rellene los campos", preferredStyle: .alert)
+            let actionAlert = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+            alertController.addAction(actionAlert)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().signIn(withEmail: self.username.text!, password: self.password.text!) { (user, error) in
+
+                if error == nil {
+                    print("Login exitoso")
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaulAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+                    alertController.addAction(defaulAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
 }
